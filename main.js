1a785f0d64748afe05d21e292be5e5cbdb6124db
Kakashi <kamuiphutko01@gmail.com>
2021-04-02 22:41:13 +0400

    change

---
+++ main.js
@@ -0,0 +1,13 @@
+function dis(value) {
+    document.getElementById("display").innerHTML += value;
+}
+
+function eqaul() {
+    let b = document.getElementById("display").innerHTML;
+    let c = eval(b);
+    document.getElementById("display").innerHTML = c;
+}
+
+function clr() {
+    document.getElementById("display").innerHTML = "";
+}
